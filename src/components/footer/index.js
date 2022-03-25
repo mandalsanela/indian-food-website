12d396e744fb8d3c5c8c0905540e4ecca16e018e
sancelaa <sanela.mandal@gmail.com>
2022-03-25 11:27:46 +0100

    scroll to top

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,13 +4,23 @@ import BackToTopIcon from "../../assets/images/icons/back-to-top.svg";
 import FacebookIcon from "../../assets/images/icons/facebook.svg";
 import InstagramIcon from "../../assets/images/icons/instagram.svg";
 const Footer = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // for smoothly scrolling
+    });
+  };
   return (
     <div className="footer-wrapper">
       <div className="logo-div">
         <img src={IndianPalace} className="logo" />
       </div>
       <div className="back-to-top-wrapper">
-        <img src={BackToTopIcon} className="back-to-top" />
+        <img
+          src={BackToTopIcon}
+          className="back-to-top"
+          onClick={scrollToTop}
+        />
         <p>Denna hemsida är skapad med hemsida är skapad</p>
       </div>
       <div className="social-media-box">
