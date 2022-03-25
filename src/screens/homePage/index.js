808f59fb3ae0a941edefaa64ae1c720d507fd222
sancelaa <sanela.mandal@gmail.com>
2022-03-25 10:17:15 +0100

    header

--- src/screens/homePage/index.js
+++ src/screens/homePage/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 import Navbar from "../../components/navbar";
-
+import HeaderPic1 from "../../assets/images/header-v1.svg";
+import HeaderPic2 from "../../assets/images/header-v2.svg";
+import Ellipse1 from "../../assets/images/ellipse1.svg";
+import Ellipse2 from "../../assets/images/ellipse2.svg";
 const HomePage = () => {
   return (
     <div className="homepage-wrapper">
-      <Navbar />
-      <div>
-        <h1>hi</h1>
+      <div className="header-wrapper">
+        <Navbar />
+        <div className="header-content">
+          <img src={HeaderPic1} className="header-pic1" />
+          <h1 className="title">Best Indian Food in Västerås</h1>
+          <img src={HeaderPic2} className="header-pic2" />
+          <div className="ellipse-row">
+            <img src={Ellipse1} />
+            <img src={Ellipse2} />
+            <img src={Ellipse2} />
+          </div>
+        </div>
       </div>
     </div>
   );
