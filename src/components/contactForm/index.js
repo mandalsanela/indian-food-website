8475dc09cc4b4247ea2894ae9fbeaaab532712f8
sancelaa <sanela.mandal@gmail.com>
2022-03-27 21:06:43 +0200

    responsiveness

--- src/components/contactForm/index.js
+++ src/components/contactForm/index.js
@@ -6,12 +6,14 @@ const ContactForm = () => {
   return (
     <div className="contact-form-wrapper">
       <div className="contact-left">
+        <div className="contact-left-text">
         <h4 className="contact-title">CONTACT US</h4>
         <h3 className="contact-subtitle">Get in touch!</h3>
         <p className="contact-text">
           Fill in a form and contact us at any time. Our support team is always
           there for you! 👋
         </p>
+        </div>
         <button className="contact-button">
           <img src={LocationIcon} className="contact-icons"/>
           <p className="contact-data">Drabantvägen 4, 194 33 Upplands Väsby, Sverige</p>
